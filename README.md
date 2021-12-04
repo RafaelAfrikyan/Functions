8d4539f41a68cb41b3ed477ce93c6f0f95d13753
RafaelAfrikyan <rafaelafrikyan95@gmail.com>
2021-12-04 23:17:22 +0400

    math operation arr element

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Given an array of numbers. Write a function to separate odd and even numbers in different arrays.
 
 [OddAndEven.js](OddAndEven.js)
+
+## 2. Mathematic operation
+
+Write a function that calculates sum, difference, multiplication and division between given array
+elements depending on passed operation symbol. Write appropriate function for each operation.
+
+[mathOperation.js](math.Operation.js)
---
+++ mathOperation.js
@@ -0,0 +1,36 @@
+//SUM
+// let a = [1, 2, 3];
+// let sum = 0;
+// function mathoperatin(sumbol, arr){
+//  if(sumbol === "+" ) {
+// for (i = 0; i < arr.length; i++) {
+//   sum = sum + arr[i];
+// }
+//   } return sum;
+//     }
+// console.log(mathoperatin("+", a));
+
+//Mult
+// let a = [2, 2, 3];
+// let mult = 1;
+// function mathoperatin(sumbol, arr){
+//  if(sumbol === "*" ) {
+// for (i = 0; i < arr.length; i++) {
+//   mult = mult * arr[i];
+// }
+//   } return mult;
+//     }
+// console.log(mathoperatin("*", a));
+
+//subtract
+
+// let a = [25, 2, 3];
+// let sub = a[0];
+// function mathoperatin(sumbol, arr){
+//  if(sumbol === "-" ) {
+// for (i = 0; i < arr.length; i++) {
+//   sub = sub - arr[i+1];
+// }
+//   } return sub;
+//     }
+// console.log(mathoperatin("-", a));
